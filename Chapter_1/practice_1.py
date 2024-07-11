52aaf0ce47dffa2b582541268d50f176e178fade
Amit Prasad <120964205+AmitPrasad212003@users.noreply.github.com>
2024-07-11 09:58:36 +0530

    Add files via upload
    Basic Question of python

---
+++ Chapter_1/practice_1.py
@@ -0,0 +1,63 @@
+# 1. Write a program to print Twinkle twinkle little star poem in python.
+
+# print('''
+# Twinkle, Twinkle, Little Star     
+#                 BY JANE TAYLOR
+      
+# Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Then the traveler in the dark
+# Thanks you for your tiny spark,
+# How could he see where to go,
+# If you did not twinkle so?
+
+# In the dark blue sky you keep,
+# Often through my curtains peep
+# For you never shut your eye,
+# Till the sun is in the sky.
+
+# As your bright and tiny spark
+# Lights the traveler in the dark,
+# Though I know not what you are,
+# Twinkle, twinkle, little star.
+# ''')
+#2. Use REPL and print the table of 5 using it. 
+# >>> for i in range(1,11): 
+# ...     print("5 *",i," = " ,i*5)
+# ...
+# 5 * 1  =  5
+# 5 * 2  =  10
+# 5 * 3  =  15
+# 5 * 4  =  20
+# 5 * 5  =  25
+# 5 * 6  =  30
+# 5 * 7  =  35
+# 5 * 8  =  40
+# 5 * 9  =  45
+# 5 * 10  =  50
+
+# 3. Install an external module and use it to perform an operation of your interest. 
+
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("Deva come here")
+# engine.runAndWait()
+
+# 4. Write a python program to print the contents of a directory using the os module.Search online for the function which does that.
+
+#import os
+
+#path = '/Users'  # Specify your directory path
+#for entry in os.listdir(path): # Use the os module to List all files and directory in the specified
+   # print(entry) # Print each files and directory name
+
+# 5. Label the program written in problem 4 with comments. 
+
