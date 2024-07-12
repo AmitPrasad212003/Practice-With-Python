9fafe4593e920dc74f155484cbed9b31efacce88
Amit Prasad <120964205+AmitPrasad212003@users.noreply.github.com>
2024-07-12 09:46:17 +0530

    Add files via upload
    practice on datatype

---
+++ chapter_2/practice.py
@@ -0,0 +1,37 @@
+
+# # 1. Write a python program to add two numbers
+# a = 5
+# b = 23
+# print(a+b)
+
+# # 2. Write a python program to find remainder when a number is divided by z.
+
+# a = 32 
+# b = 5 
+# print("Reaminder when a is divided by b is : ",a%b)
+
+# # 3. Check the type of variable assigned using input () function.
+
+# a = input("Enter the Value of a : ")
+# print(type(a))
+
+# # 4. Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b = 80
+
+# a = int(input("Enter number 1 : "))
+# b = int(input("Enter number 2 : "))
+
+# print("a is greater than d is : ", a>b)
+
+# # 5. Write a python program to find an average of two numbers entered by the user.
+
+# a = int(input("Enter number 1 : "))
+# b = int(input("Enter number 2 : "))
+
+# print("Average of two number is : ", (a+b)/2)
+
+# # 6. Write a python program to calculate the square of a number entered by the user.
+# a = int(input("Enter your number : "))
+
+# print("The square ",a," is : ",a**2)
+
+
