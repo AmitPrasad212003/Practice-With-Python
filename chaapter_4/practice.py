92c5d1aa3cee81cf8e39775e877ccd8746ab7623
Amit Prasad <120964205+AmitPrasad212003@users.noreply.github.com>
2024-07-21 14:12:56 +0530

    practice with file

---
+++ chaapter_4/practice.py
@@ -0,0 +1,61 @@
+# 1. Write a program to store seven fruits in a list entered by the user.
+
+# fruits = []
+
+# f1 = int(input("Enter Fruit Name here : "))
+# fruits.append(f1)
+# f2 = int(input("Enter Fruit Name here : "))
+# fruits.append(f2)
+# f3 = int(input("Enter Fruit Name here : "))
+# fruits.append(f3)
+# f4 = int(input("Enter Fruit Name here : "))
+# fruits.append(f4)
+# f5 = int(input("Enter Fruit Name here : "))
+# fruits.append(f5)
+# f6 = int(input("Enter Fruit Name here : "))
+# fruits.append(f6)
+# f7 = int(input("Enter Fruit Name here : "))
+# fruits.append(f7)
+
+# print(fruits)
+
+# 2. Write a program to accept marks of 6 students and display them in a sorted manner
+
+# marks = []
+
+# s1 = int(input("Enter student 1 marks here : "))
+# marks.append(s1)
+# s2 = int(input("Enter student 2 marks here : "))
+# marks.append(s2)
+# s3 = int(input("Enter student 3 marks here : "))
+# marks.append(s3)
+# s4 = int(input("Enter student 4 marks here : "))
+# marks.append(s4)
+# s5 = int(input("Enter student 5 marks here : "))
+# marks.append(s5)
+# s6 = int(input("Enter student 6 marks here : "))
+# marks.append(s6)
+# s7 = int(input("Enter student 7 marks here : "))
+# marks.append(s7)
+# marks.sort()
+# print(marks)
+
+# 3. Check that a tuple type cannot be changed in python.
+
+# a = (34,234,"AMIT")
+
+# a[2] = "Prasad"
+
+# print(a) # not possible because tuple is immutable data type
+
+# 4. Write a program to sum a list with 4 numbers.
+
+l = [98,97,76,78,76,89]
+
+print(sum(l))
+
+# 5. Write a program to count the number of zeros in the following tuple:
+
+a = (7, 0, 8, 0, 0, 9)
+
+print(a.count(0))
