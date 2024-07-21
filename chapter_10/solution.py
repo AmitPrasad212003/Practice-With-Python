453674e0b92b481417ca36ee6eddfcffcfb7c5fb
Amit Prasad <120964205+AmitPrasad212003@users.noreply.github.com>
2024-07-21 14:08:32 +0530

    practice with oops

--- chapter_10/solution.py
+++ chapter_10/solution.py
@@ -1,120 +1,179 @@
-# 1. Create a class “Programmer” for storing information of few programmers working at Microsoft.
+# 1. Create a class (2-D vector) and use it to create another class representing a 3-Dvector.
 
-# class Programmer:
-#     company = "Microsoft"
+# class Two_D_Vector :
+#     def __init__(self,i,j):
+#         self.i = i
+#         self.j = j
 
-#     def __init__(slf,name, salary, pin):
-#         slf.name = name
-#         slf.salary = salary
-#         slf.pin = pin
-
-# p = Programmer("Harry",1200000,123123)
-# # print(p.company,"\t",p.name,"\t",p.salary,"\t",p.pin)
-# print(f"{p.company}  {p.name}  {p.salary}  {p.pin}")
-# r = Programmer("Amit",2000000,234123)
-# print(f"{r.company}  {r.name}  {r.salary}  {r.pin}")
+#     def show(self):
+#         print(f"The vevtor is {self.i}i + {self.j}j ")
 
 
-# 2. Write a class “Calculator” capable of finding square, cube and square root of a number
+# class Three_D_Vector(Two_D_Vector) :
+#     def __init__(self,i,j,k):
+#         super().__init__(i,j)
+#         self.k = k
 
-# class Calculator:
-#     def __init__(slf,n):
-#         slf.n = n
+#     def show(self):
+#         print(f"The vevtor is {self.i}i + {self.j}j + {self.k}k")
 
-#     def square(slf):
-#         print(f"The square is {slf.n*slf.n}")
-
-#     def cube(slf):
-#         print(f"The cube is {slf.n*slf.n*slf.n}")
-
-#     def squareroot(slf):
-#         # print(slf.n)
-#         print(f"The squareroot is {slf.n**1/2}")
-
-# a = Calculator(4)
-# a.square()
-# a.cube()
-# a.squareroot()
+# a = Two_D_Vector(1,2)
+# a.show()
+# b = Three_D_Vector(1,2,3)
+# b.show()
 
 
-# 3. Create a class with a class attribute a; create an object from it and set ‘a’ directly using ‘object.a = 0’. Does this change the class attribute?
+# 2. Create a class ‘Pets’ from a class ‘Animals’ and further create a class ‘Dog’ from ‘Pets’. Add a method ‘bark’ to class ‘Dog’.
 
+# class Animals:
+#     pass
 
-# class Demo:
-#     a =  4
+# class Pets(Animals):
+#     pass 
 
-# o = Demo()
-# print(o.a)# Print the class attribute because instance attribute is not present 
-# o.a = 0 
-# print(o.a)# Print the instance attribute because instance attribute is present 
-
-# print(Demo.a) # print the class attribute
-# # The ans. is NO
-
-
-# 4. Add a static method in problem 2, to greet the user with hello.
-
-
-# class Calculator:
-#     def __init__(slf,n):
-#         slf.n = n
-
-#     def square(slf):
-#         print(f"The square is {slf.n*slf.n}")
-
-#     def cube(slf):
-#         print(f"The cube is {slf.n*slf.n*slf.n}")
-
-#     def squareroot(slf):
-#         # print(slf.n)
-#         print(f"The squareroot is {slf.n**1/2}")
+# class Dog(Pets):
     
 #     @staticmethod
-#     def hello():
-#         print("Hello there!")
-
-# a = Calculator(4)
-# a.square()
-# a.cube()
-# a.squareroot()
-# a.hello()
+#     def bark():
+#         print("Bow Bow!")
 
 
-# 5. Write a Class ‘Train’ which has methods to book a ticket, get status (no of seats) and get fare information of train running under Indian Railways
-# from random import randint
-# class Train:
-#     def __init__(slf,trainNo):
-#         slf.trainNo = trainNo
-#     def book(slf,fro,to):
-#         print(f"Ticket is booked in train no : {slf.trainNo} from {fro} to {to}")
+# d = Dog()
+# d.bark()
 
-#     def getStatus(slf):
-#         print(f"Train no : {slf.trainNo} is running on time")
 
-#     def getFare(slf,fro,to):
-#          print(f"Ticket is fare in train no : {slf.trainNo}\nfrom {fro} to {to} is : {randint(222,5567)}")
+# 3. Create a class ‘Employee’ and add salary and increment properties to it.
+ #Write a method ‘salaryAfterIncrement’ method with a @property decorator with a setter which changes the value of increment based on the salary
+
+# class Employee():
+#     salary = 23
+#     increment = 20
+
+#     @property
+#     def salaryAfterIncrement(self):
+#         return (self.salary + self.salary * (self.increment/100))
     
-# t = Train(345231)
-# t.book("Randnagar","Randipaga")
-# t.getFare("Randnagar","Randipaga")
-# t.getStatus()
+#     @salaryAfterIncrement.setter
+#     def salaryAfterIncrement(self,salary):
+#         self.increment = ((salary/self.salary) - 1)*100
 
-# 6. Can you change the slf-parameter inside a class to something else (say “harry”). Try changing slf to “slf” or “harry” and see the effects.
+# e = Employee()
 
-from random import randint
-class Train:
-    def __init__(slf,trainNo):
-        slf.trainNo = trainNo
-    def book(slf,fro,to):
-        print(f"Ticket is booked in train no : {slf.trainNo} from {fro} to {to}")
+# # print(e.salaryAfterIncrement)
 
-    def getStatus(slf):
-        print(f"Train no : {slf.trainNo} is running on time")
+# e.salaryAfterIncrement = 280
 
-    def getFare(slf,fro,to):
-         print(f"Ticket is fare in train no : {slf.trainNo}\nfrom {fro} to {to} is : {randint(222,5567)}")
+# print(e.increment)
+
+
+# 4. Write a class ‘Complex’ to represent complex numbers, along with overloaded operators ‘+’ and ‘*’ which adds and multiplies them.
+
+
+# class Complex:
+#     def __init__(self, r, i):
+#         self.r = r
+#         self.i = i
+
+#     def __add__(self,c2):
+#         return Complex(self.r + c2.r , self.i + c2.i)
     
-t = Train(345231)
-t.book("Randnagar","Randipaga")
-t.getFare("Randnagar","Randipaga")
-t.getStatus()
+#     def __mul__(self, c2):
+#         real_part = self.r * c2.r - self.i * c2.i
+#         imaginary_part = self.r * c2.i + self.i * c2.r
+#         return Complex(real_part, imaginary_part)
+    
+#     def __str__(self):
+#         return f"{self.r} + {self.i}i"
+        
+    
+# c1 = Complex(1,2)
+# c2 = Complex(3,4)
+
+# print(c1+ c2)
+# print(c1* c2)
+
+
+# 5. Write a class vector representing a vector of n dimensions. Overload the + and * operator which calculates the sum and the dot(.) product of them.
+
+
+# class Vector:
+#     def __init__(self,x,y,z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+
+#     def __add__(self, other):
+#         result = (self.x + other.x + self.y + other.y + self.z + other.z )
+#         return result
+    
+#     def __mul__(self, other):
+#         result = (self.x * other.x + self.y * other.y + self.z * other.z )
+#         return result
+    
+#     def __str__(self):
+#         return f"Vector({self.x}, {self.y}, {self.z})"
+    
+    
+# # Test the implementation 
+
+# v1 = Vector(1,2,3)
+# v2 = Vector(4,5,6)
+# v3 = Vector(7,8,9)
+
+# print(v1 + v2)
+# print(v1 + v2 )
+
+# print(v1 + v3)
+# print(v1 * v3)
+    
+
+
+# 6. Write __str__() method to print the vector as follows:
+
+
+# class Vector:
+#     def __init__(self,x,y,z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+
+#     def __add__(self, other):
+#         result = Vector(self.x + other.x , self.y + other.y , self.z + other.z) 
+#         return result
+    
+#     def __mul__(self, other):
+#         result = self.x * other.x + self.y * other.y + self.z * other.z 
+#         return result
+    
+#     def __str__(self):
+#         return f" {self.x}i +  {self.y}j + {self.z}k "
+    
+    
+# # Test the implementation 
+
+# v1 = Vector(1,2,3)
+# v2 = Vector(4,5,6)
+# v3 = Vector(7,8,9)
+
+# print(v1 + v2)
+# print(v1 * v2 )
+
+# print(v1 + v3)
+# print(v1 * v3)
+    
+
+
+# 7. Override the __len__() method on vector of problem 5 to display the dimension of the vector.
+
+class Vector:
+    def __init__(self,l):
+        self.l = l
+    
+    def __len__(self):
+        return len(self.l)
+    
+    
+# Test the implementation 
+
+v1 = Vector([1,2,3,4,5,6])
+print(len(v1))
